51596f77cd488a9b1d4983b8a80268381a8beaa2
Vikash Khati <vikashkhati007@gmail.com>
2024-10-03 17:43:49 +0530

    desgine issue fixed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,9 +6,9 @@ import TrendingProductsHero from "@/components/Trendingproducts";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screenbg-gray-50">
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 sm:pb-24">
         <Hero />
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8 sm:pb-24 ">
         <TrendingProductsHero/>
         <EcommerceFeatures />
         <FeaturedCategories />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Hero() {
   return (
-    <section className="relative bg-gradient-to-b from-indigo-600 to-indigo-800 text-white overflow-hidden w-full mb-5">
+    <section className="relative bg-gradient-to-b from-indigo-600 to-indigo-800 text-white overflow-hidden w-full my-5">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-24 sm:py-32 relative z-10">
         <div className="text-center max-w-3xl mx-auto">
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold tracking-tight mb-4">
