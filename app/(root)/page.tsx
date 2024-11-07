75a35a34aafbcf3576e96d48025657f0194384f4
Vikash Khati <vikashkhati007@gmail.com>
2024-11-07 16:02:25 +0530

    fix hero ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,10 @@ import TrendingProductsHero from "@/components/Trendingproducts";
 export default function HomePage() {
   return (
     <div className="min-h-screenbg-gray-50">
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8 sm:pb-24">
+      <main className="w-full ">
         <Hero />
+      </main>
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 sm:pb-24 w-full">
         <TrendingProductsHero/>
         <EcommerceFeatures />
         <FeaturedCategories />
